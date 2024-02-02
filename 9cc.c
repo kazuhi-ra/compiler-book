c6803d5b9384340208cd131927bd853f4534fdba
kazuhi-ra <kazuhira4126@gmail.com>
2024-02-02 02:58:38 +0000

    空白文字を読み飛ばすように

--- 9cc.c
+++ 9cc.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,6 +18,11 @@ int main(int argc, char **argv) {
   printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
   while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
     if (*p == '+') {
       p++;
       printf("  add rax, %ld\n", strtol(p, &p, 10));
